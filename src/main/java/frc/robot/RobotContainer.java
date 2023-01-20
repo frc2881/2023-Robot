a9ae40476d11eb400502467538939389ead76213
Lady Cans <frc2881@gmail.com>
2023-01-19 20:01:05 -0600

    Added DriveWithJoysticks
    student: Ace
    mentor: Andrew

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,18 +5,37 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.commands.drive.DriveWithJoysticks;
 import frc.robot.subsystems.drive.Drive;
 
 public class RobotContainer {
+  private final XboxController m_driverController = new XboxController(0);
+  private final XboxController m_manipulatorController = new XboxController(1);
   Drive m_drive = new Drive();
+
+  private final DriveWithJoysticks m_driveWithJoysticks = new DriveWithJoysticks(
+      m_drive,
+      () -> applyDeadband(-m_driverController.getLeftY()),
+      () -> applyDeadband(m_driverController.getRightX()));
 
   public RobotContainer() {
     configureBindings();
+    m_drive.setDefaultCommand(m_driveWithJoysticks);
   }
 
-  private void configureBindings() {}
+  public double applyDeadband(double input) {
+    if (Math.abs(input) < 0.1) {
+      return 0.0;
+    } else {
+      return input;
+    }
+  }
+
+  private void configureBindings() {
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
