81337dc58ed19e0f95cdac8fd00012043718951d
ladycans <frc2881@gmail.com>
2023-01-21 18:38:05 -0600

    Working more on the command S: Fae M: Robert, Brian

--- src/main/java/frc/robot/subsystems/Drive.java
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -4,6 +4,10 @@
 
 package frc.robot.subsystems;
 
+import com.pathplanner.lib.PathPlannerTrajectory;
+import com.pathplanner.lib.commands.PPSwerveControllerCommand;
+
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
@@ -12,10 +16,12 @@ import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 import frc.robot.Constants.DriveConstants;
 import frc.robot.utils.NavX;
 import frc.robot.utils.SwerveModule;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Drive extends SubsystemBase {
   // Create SwerveModules
@@ -110,6 +116,19 @@ public class Drive extends SubsystemBase {
             m_rearRight.getPosition()
         },
         pose);
+  }
+  
+  public Command FollowTrajectory (PathPlannerTrajectory path) {
+    return new PPSwerveControllerCommand(
+      path,
+      this::getPose,
+      new PIDController(Constants.Trajectory.xP, Constants.Trajectory.xI, Constants.Trajectory.xD),
+      new PIDController(Constants.Trajectory.yP, Constants.Trajectory.yI, Constants.Trajectory.yD),
+      new PIDController(Constants.Trajectory.tP, Constants.Trajectory.tI, Constants.Trajectory.tD),
+      this::setModuleStates, // Needs to have a supplier
+      true,
+      this 
+      );
   }
 
   /**
