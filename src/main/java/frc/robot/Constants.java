b6b4fa4d6f5a0afb6c6cf3707923a8a5f5f5532a
Lady Cans <frc2881@gmail.com>
2023-02-04 16:54:59 -0600

    Stub out clean vision with pose estimator
    students: Ace, Sahasra
    Mentor: Robert

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,7 +7,10 @@ package frc.robot;
 
 import com.revrobotics.CANSparkMax.IdleMode;
 
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.util.Units;
 
@@ -190,7 +193,24 @@ public class Constants {
 
       }
 
-      public static final class VisionTracking {
+      public static final class Field {
+        public static final double kLength = Units.feetToMeters(54);
+        public static final double kWidth = Units.feetToMeters(27);
+        }
+
+      public static final class Vision {
+
+        public static final String kLeftCameraName = "LEFT";
+        public static final Transform3d kLeftRobotToCam =
+            new Transform3d(
+                new Translation3d(0.5, 0.0, 0.5),
+                new Rotation3d(0, 0,0)); 
+
+        public static final String kRightCameraName = "RIGHT";
+        public static final Transform3d kRightRobotToCam =
+            new Transform3d(
+                new Translation3d(0.5, 0.0, 0.5),
+                new Rotation3d(0, 0,0)); 
 
       }
 
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,7 +8,6 @@ package frc.robot;
 import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.utils.Log;
@@ -18,7 +17,6 @@ public class Robot extends TimedRobot {
   private static Robot m_robotInstance;
   private Command m_autonomousCommand;
   private RobotContainer m_robotContainer;
-
 
   @Override
   public void robotInit() {
