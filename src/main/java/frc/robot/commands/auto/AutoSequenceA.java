8f369bb45b81b29c7dbc628eb5acbe0c9b52b259
ladycans <frc2881@gmail.com>
2023-02-25 17:32:52 -0600

    edited auto Fae; Robert

--- src/main/java/frc/robot/commands/auto/AutoSequenceA.java
+++ src/main/java/frc/robot/commands/auto/AutoSequenceA.java
@@ -8,16 +8,17 @@ package frc.robot.commands.auto;
 import com.pathplanner.lib.PathPlannerTrajectory;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.subsystems.Arm;
+import frc.robot.subsystems.ArmExtension;
+import frc.robot.subsystems.ArmTilt;
 import frc.robot.subsystems.Drive;
 import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Suction;
 
 public class AutoSequenceA extends SequentialCommandGroup {
-  public AutoSequenceA(Drive drive, Suction suction, Arm arm, Intake intake, PathPlannerTrajectory trajectory) {
+  public AutoSequenceA(Drive drive, Suction suction, ArmExtension armExtension, ArmTilt armTilt, Intake intake, PathPlannerTrajectory trajectory) {
     addCommands(
-      new AutoScoreHigh(suction, arm, intake),
-      new AutoDriveFlat(drive, arm, intake, trajectory)
+      new AutoScoreHigh(suction, armExtension, armTilt, intake),
+      new FollowTrajectory(trajectory, true, drive)
     );
   }
 }
